a358ab813ae78711e877f3695dc10c9f22ca8982
Ebuall <gusliyar@mail.ru>
2019-09-06 19:54:06 +0300

    Updates

---
+++ precache-manifest.7ab98350607dab1387ab7204642dd7e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a67d203f77bc10a0c66832e73983c689",
+    "url": "/pwmap/index.html"
+  },
+  {
+    "revision": "a05e7c9e016bf27045d7",
+    "url": "/pwmap/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "021922edc37276c147f1",
+    "url": "/pwmap/static/js/2.9951dd35.chunk.js"
+  },
+  {
+    "revision": "a05e7c9e016bf27045d7",
+    "url": "/pwmap/static/js/main.0d3e7b45.chunk.js"
+  },
+  {
+    "revision": "dcb764272410f2ec0332",
+    "url": "/pwmap/static/js/runtime~main.1d4a43a6.js"
+  }
+]);
